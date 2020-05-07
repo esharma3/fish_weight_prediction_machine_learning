fa871a338f1cabc01990f708cb14eb2cec8aaa01
Ekta Sharma <ekta.sharma3@gmail.com>
2020-05-07 15:13:23 -0500

    final version. cleaned the code

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,10 @@
-import numpy as np
-from sklearn.compose import ColumnTransformer 
-from sklearn.preprocessing import OneHotEncoder
 
-def prep_data(df):
+
+###########################################################################################################################################################
+#														function to handle outliers (applied ONLY to training data)										  #
+###########################################################################################################################################################
+
+def handle_outlier(df):
 
 	numerical = ["Weight", "Length1", "Length2", "Length3", "Height", "Width"]
 
@@ -16,33 +18,26 @@ def prep_data(df):
 		df1_upperend = df1_Q3 + (1.5 * df1_IQR)
 
 		df1_outliers = df1[(df1 < df1_lowerend) | (df1 > df1_upperend)]
-		# print(df1_outliers)
+		print(df1_outliers)    # <--- this printed row # 13
 
-    # dropping the outlier at row # 13
-	# df = df.drop([13])
+	# dropping the outlier at row # 13
+	df = df.drop([13])
 
-	# applying log conversion to normalizes the distribution of features and the target variable to some extent
-	df["Weight"] = np.log(df["Weight"])
-	df["Length1"] = np.log(df["Length1"])
-	df["Length2"] = np.log(df["Length2"])
-	df["Length3"] = np.log(df["Length3"])
-	df["Height"] = np.log(df["Height"])
-	df["Width"] = np.log(df["Width"])
+	return df
 
-	# since the 3 lenght variables are highly correlated and Length3 values are the highest for each row, it is probably the Total Length of the fish. 
+
+###########################################################################################################################################################
+#																 function to prep data for prediction													  #
+###########################################################################################################################################################
+
+def prep_data(df):
+
+	# The 3 length variables are highly collinear. Length3 shows highest correlation with target variable and Length3 values are the highest for each row, it is probably the Total Length of the fish. 
 	# So keeping Length3 and dropping the other two length variables from the dataset.
 	df = df.drop(["Length1", "Length2"], axis=1)
 
-	# dividing the data into features and target variable
-	X = df.drop("Weight", axis=1).values
-	y = df["Weight"].values
-
-	# encoding the categorical feature - species
-	ct = ColumnTransformer(
-	    [("one_hot_encoder", OneHotEncoder(categories="auto"), [0])],
-	    remainder="passthrough",
-	)
-
-	X = ct.fit_transform(X)
+	# Dividing the data into features and target variable
+	X = df.drop("Weight", axis=1)
+	y = df["Weight"]
 
 	return X, y
