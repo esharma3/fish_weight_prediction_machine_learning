647477b785d0e900ee254f85c5c6639214704967
Ekta Sharma <ekta.sharma3@gmail.com>
2020-05-07 16:14:07 -0500

    final formatted code

--- train.py
+++ train.py
@@ -1,26 +1,31 @@
 ################################################################################################
-#						    		 importing dependencies here                               #
+# 						    		 importing dependencies here                               #
 ################################################################################################
 
 import numpy as np
 import pandas as pd
+
 # for data prep
 from preprocess import prep_data, handle_outlier
+
 # for encoding and transformation
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import TransformedTargetRegressor
+
 # for building pipeline
 from sklearn.pipeline import make_pipeline
+
 # model
 from sklearn.linear_model import LinearRegression
+
 # for saving the model
 from joblib import dump
 
 ################################################################################################
-#											MAIN 											   #
-################################################################################################	
+# 											MAIN 											   #
+################################################################################################
 
 df = pd.read_csv("fish_participant.csv")
 
@@ -28,7 +33,7 @@ df = handle_outlier(df)
 
 X, y = prep_data(df)
 
-# for encoding the categorical feature(species) and for transforming the numerical features 
+# for encoding the categorical feature(species) and for transforming the numerical features
 numerical = ["Length3", "Height", "Width"]
 categorical = ["Species"]
 
