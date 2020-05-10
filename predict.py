98522837872b794a00b7cf7f1dcbc80addce2b4c
Ekta Sharma <ekta.sharma3@gmail.com>
2020-05-09 21:37:02 -0500

    final formatted code

--- predict.py
+++ predict.py
@@ -44,10 +44,12 @@ if __name__ == "__main__":
     print(r2_score)
 
 
-### WE WRITE THIS ###
-# from sklearn.metrics import mean_squared_error
+################################################################################################
+#                          Testing using "fish_holdout.csv"                                    #
+################################################################################################
+
 # ho_predictions = predict_from_csv("fish_holdout.csv")
 # ho_truth = pd.read_csv("fish_holdout.csv")["Weight"].values
 # ho_mse = mean_squared_error(ho_truth, ho_predictions)
 # print(ho_mse)
-######
+
